3088b15dec90957d051e7e68a006ae68d8d3c3c7
黄兴 <huangxing03@corp.netease.com>
2022-02-21 16:57:07 +0800

    linker wrapper

--- build/toolchain/win/toolchain.py
+++ build/toolchain/win/toolchain.py
@@ -31,6 +31,16 @@ MSVC_TOOLSET_VERSION = {
 }
 
 
+def GetEnv(arch):
+    """Gets the saved environment from a file for a given architecture."""
+    # The environment is saved as an "environment block" (see CreateProcess
+    # and msvs_emulation for details). We convert to a dict here.
+    # Drop last 2 NULs, one for list terminator, one for trailing vs. separator.
+    pairs = open(arch).read()[:-2].split('\0')
+    kvs = [item.split('=', 1) for item in pairs]
+    return dict(kvs)
+
+
 def DetectVisualStudioPath():
     """Return path to the installed Visual Studio."""
 
